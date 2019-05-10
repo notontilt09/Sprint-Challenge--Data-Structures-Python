d7e5135cfe9b3217a88a78432cdb4abbccf77701
Daniel Weinman <namniewd@gmail.com>
2019-05-10 11:42:01 -0400

    got stretch to work with O(n log(n)) with only lists

--- names/names.py
+++ names/names.py
@@ -37,17 +37,47 @@ Below is for loop with a nested BST which is O(n log(n)) runtime
 """
 Below is hash table to try and get runtime to O(2n) = O(n)
 """
-# initialize hash table
-lookup = {}
+# # initialize hash table
+# lookup = {}
 
-# loop through names1 and add keys to hash table for each name.  Value is irrelevant, we just care about the keys.
-for i in names_1:
-    lookup[i] = 1
+# # loop through names1 and add keys to hash table for each name.  Value is irrelevant, we just care about the keys.
+# for i in names_1:
+#     lookup[i] = 1
 
-for j in names_2:
-    if j in lookup.keys():
-        duplicates.append(j)
+# for j in names_2:
+#     if j in lookup.keys():
+#         duplicates.append(j)
 
+
+"""
+Stretch:  Can only use lists
+"""
+# This is about 5x faster than original solution.  I'm guessing because the nested loop is python optimized search.
+# for i in names_1:
+#     if i in names_2:
+#         duplicates.append(i)
+
+
+# I think a faster way might be sorting one of the lists, then performing binary search for each name in 2nd list.
+# Should be O(n log(n)) if done correctly
+sorted_1 = sorted(names_1)
+
+def search(value, arr, left, right):
+    # binary search algo
+    if left > right:
+        return None
+    midpoint = left + (right - left) // 2
+    if arr[midpoint] == value:
+        return value
+    elif arr[midpoint] > value:
+        return search(value, arr, left, midpoint - 1)
+    elif arr[midpoint] < value:
+        return search(value, arr, midpoint + 1, right)
+
+for i in names_2:
+    if search(i, sorted_1, 0, len(sorted_1) - 1) is not None:
+        duplicates.append(i)
+    
 
 
 
